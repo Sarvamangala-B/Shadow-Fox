26864076d0af52677e0465c3b07fac19734bd326
Sarvamangala.B <sarvamangala3182004@gmail.com>
2025-08-10 21:01:23 +0530

    task 1

--- TASK 1/Variables/Task(variables).py
+++ TASK 1/Variables/Task(variables).py
@@ -1,5 +1,14 @@
 # Begginers Task 1 - Variables
 
+# 1. Storing Pi value
+pi = 22/7
+print("pi:", pi, "type:", type(pi))
+
+# 2. Invalid Syntax
+# We cannot use 'for' as a variable name because it's reserved
+for = 4
+print("for:", for)
+
 # 3. Simple Interest calculation
 P = 100  # principal amount
 R = 2     # rate of interest
