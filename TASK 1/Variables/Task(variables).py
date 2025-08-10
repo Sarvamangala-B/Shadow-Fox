8b4153b818f976d3342bdada1a9029e936179e20
Sarvamangala.B <sarvamangala3182004@gmail.com>
2025-08-10 20:51:17 +0530

    Add Beginner Task 1 variables and screenshot

---
+++ TASK 1/Variables/Task(variables).py
@@ -0,0 +1,8 @@
+# Begginers Task 1 - Variables
+
+# 3. Simple Interest calculation
+P = 100  # principal amount
+R = 2     # rate of interest
+T = 4     # time in years
+SI = P * R * T / 100
+print("Simple Interest for 3 years:", SI)
